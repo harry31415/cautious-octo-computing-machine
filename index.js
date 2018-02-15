222de2a9a564d65980803717731e92b4c6c28192
Harry Sidiropoulos <harry@microlab.ntua.gr>
2018-02-15 11:57:23 +0200

    Client test repo

---
+++ index.js
@@ -0,0 +1,61 @@
+var io = require('socket.io-client');
+var ss = require('socket.io-stream');
+var fs = require('fs');
+const path = require('path');
+var AdmZip = require('adm-zip');
+
+
+var argv = require('minimist')(process.argv.slice(2));
+console.dir(argv);
+
+var filename =argv._[0] ;
+var port = argv.p || 3636;
+
+console.log('Filename: '+filename+' Port: '+port);
+
+
+var socket = io.connect('http://localhost:'+port);
+var stream = ss.createStream();
+
+const ip = require('ip');
+
+let myIP=ip.address();
+
+let user={
+    ip:myIP,
+    token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9',
+    type:'premium'
+}
+
+socket.emit('tokenID', user);
+
+
+ss(socket).emit('scriptPY', stream, {name: filename});
+fs.createReadStream(filename).pipe(stream);
+socket.on('outPy', function(msg){
+    console.log("Received: " + msg);
+});
+
+ss(socket).on('results', function(stream, data) {
+    var filename = path.basename(data.name);
+    stream.pipe(fs.createWriteStream(filename));
+    stream.on('finish', () => {
+        console.log('Got File from stream :'+filename);
+        var zip = new AdmZip(filename);
+        var zipEntries = zip.getEntries();
+        zipEntries.forEach(function(zipEntry) {
+            let name=zipEntry.entryName;
+            let sizeuncomp=formatBytes(zipEntry.header.size);
+            let sizecomp=formatBytes(zipEntry.header.compressedSize);
+            console.log(name+": "+sizeuncomp+" --> "+sizecomp); // outputs zip entries information
+        });
+        process.exit();
+    });
+
+});
+
+function formatBytes(a,b){
+    if(0==a)return"0 Bytes";
+    var c=1024,d=b||2,e=["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"],f=Math.floor(Math.log(a)/Math.log(c));
+    return parseFloat((a/Math.pow(c,f)).toFixed(d))+" "+e[f]
+}
---
+++ tests/down.py
@@ -0,0 +1,43 @@
+import time
+import numpy as np
+import random
+import string
+SUFFIXES = {1000: ['KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+            1024: ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']}
+
+def approximate_size(size, a_kilobyte_is_1024_bytes=True):
+    '''Convert a file size to human-readable form.
+
+    Keyword arguments:
+    size -- file size in bytes
+    a_kilobyte_is_1024_bytes -- if True (default), use multiples of 1024
+                                if False, use multiples of 1000
+
+    Returns: string
+
+    '''
+    if size < 0:
+        raise ValueError('number must be non-negative')
+
+    multiple = 1024 if a_kilobyte_is_1024_bytes else 1000
+    for suffix in SUFFIXES[multiple]:
+        size /= multiple
+        if size < multiple:
+            return '{0:.1f} {1}'.format(size, suffix)
+
+    raise ValueError('number too large')
+
+if __name__ == '__main__':
+    print(approximate_size(1000000000000, False))
+    print(approximate_size(1000000000000))
+    print (time.strftime("%H:%M:%S"))
+    name = time.strftime("%H:%M:%S")
+    n=102;
+    lorem="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+
+    text= "".join( [lorem for i in range(n)] )
+    f = open(name+".ppp",'w')
+    f.write(text)
+    f.close()
+
+
